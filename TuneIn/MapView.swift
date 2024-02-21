c7ac700e21c6ba13554e788d758a3b72df7734b4
Bert Joseph Prestoza <bertjosephprestoza@gmail.com>
2024-02-21 10:18:07 -0800

    Initial commit

---
+++ TuneIn/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  TuneIn
+//
+//  Created by Bert Joseph Prestoza on 2/21/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map View")
+    }
+}
+
+#Preview {
+    MapView()
+}
