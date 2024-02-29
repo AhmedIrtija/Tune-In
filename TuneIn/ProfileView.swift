17df65f2f8c8355801cf77ab2c7868cbf0f42e2f
Nivrithi Krishnan <nivrithi@soumya.com>
2024-02-29 13:01:51 -0800

    basic layout

--- TuneIn/ProfileView.swift
+++ TuneIn/ProfileView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 struct ProfileView: View {
     @Binding var rootViewType: RootViewType
     var body: some View {
-        Text("Profile View")
+        ZStack() {
+            Color.black
+                .ignoresSafeArea()
+            VStack() {
+                //Display Name
+                Text("TuneIn")
+                    .font(Font.custom("Damion", size: 40))
+                    .padding([.bottom], 10)
+                    .foregroundColor(.white)
+                //Pronouns
+                Text("(she/her)")
+                    .font(.custom("Helvetica", size: 14))
+                    .padding([.bottom], 5)
+                //Location
+                Text("Davis, CA, USA")
+                    .padding([.bottom], 40)
+                //Bio
+                Text("Hey there! I am using TuneIn.")
+                    .padding([.bottom], 10)
+            }
+            .font(.custom("Helvetica", size: 18))
+            .padding([.horizontal], 30)
+            .foregroundColor(Colors.gray)
+        }
     }
 }
 
