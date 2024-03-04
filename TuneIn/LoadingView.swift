0e381d3835d6ff77130a01586ae9cbec3a3b89b2
ashanisinha <ashani.sinha@gmail.com>
2024-03-03 22:00:52 -0800

    trying a web model...must be tested

--- TuneIn/LoadingView.swift
+++ TuneIn/LoadingView.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct LoadingView : View {
+    
+    @State var openView = false
+    var finalURL = SpotifyController.shared.getAccessTokenURL()
+    
+    var body: some View {
+        ZStack {
+            Text("Loading...")
+            if let unwrappedURLRequest = finalURL {
+                if let unwrappedURL = unwrappedURLRequest.url {
+                    WebView(url: unwrappedURL)
+                        .onAppear {
+                            Task {
+                                //load data on appear
+                                // apply an overlay for loading screen
+                                let song = try await SpotifyController.shared.getCurrentlyPlayingTrack()
+                                print(song?.name as Any)
+                            }
+                        }
+                }
+            }
+        }
+        .overlay {
+                ZStack {
+                    Color(white: 0, opacity: 0.75)
+                    ProgressView().tint(.white)
+                }
+        }
+    }
+}
--- TuneIn/WebModel.swift
+++ TuneIn/WebModel.swift
@@ -10,17 +10,29 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
- let url: URL
- func makeUIView(context: Context) -> WKWebView  {
-     
-     let wkwebView = WKWebView()
-     let request = URLRequest(url: url)
-     wkwebView.load(request)
-     return wkwebView
- }
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
     
- func updateUIView(_ uiView: WKWebView, context: Context) {
-     
- }
+        let url: URL
+        
+        func makeUIView(context: Context) -> WKWebView  {
+            
+            let wkwebView = WKWebView()
+            let request = URLRequest(url: url)
+            wkwebView.load(request)
+            return wkwebView
+        }
     
+    func makeCoordinator() -> WebCoordinator{
+        WebCoordinator (didStart: <#T##() -> Void#>, didFinish: <#T##() -> Void#>)
+    }
+    }
+
+class WebCoordinator: NSObject, WKNavigationDelegate {
+    var didStart: () -> Void
+    var didFinish: () -> Void
+    
+    init(didStart: @escaping () -> Void, didFinish: @escaping () -> Void) {
+        self.didStart = didStart
+        self.didFinish = didFinish
+    }
 }
