f4a04d55ccb07480875ac9afaa9af7151f1acd46
Bert Joseph Prestoza <bertjosephprestoza@gmail.com>
2024-03-08 15:56:31 -0800

    Implement AuthenticationManager for Firebase authentication

--- TuneIn/AuthenticationManager.swift
+++ TuneIn/AuthenticationManager.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+struct AuthDataResultModel {
+    let uid: String
+    let email: String?
+    let photoUrl: String?
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.photoUrl = user.photoURL?.absoluteString
+    }
+}
+
+final class AuthenticationManager {
+    static let shared = AuthenticationManager()
+    private init() { }
+    
+    func getAuthenticatedUser() throws -> AuthDataResultModel {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return AuthDataResultModel(user: user)
+    }
+    
+    func createUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    func signOut() throws {
+        try Auth.auth().signOut()
+    }
+}
