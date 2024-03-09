c5fb06b8df469814f3a5af9cda4d650cf43dad06
Bert Joseph Prestoza <bertjosephprestoza@gmail.com>
2024-03-08 23:28:23 -0800

    Implement sign in, password functions

--- TuneIn/AuthenticationManager.swift
+++ TuneIn/AuthenticationManager.swift
@@ -32,9 +32,27 @@ final class AuthenticationManager {
         return AuthDataResultModel(user: user)
     }
     
+    @discardableResult
     func createUser(email: String, password: String) async throws -> AuthDataResultModel {
         let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
         return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    @discardableResult
+    func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    func resetPassword(email: String) async throws {
+        try await Auth.auth().sendPasswordReset(withEmail: email)
+    }
+    
+    func updatePassword(password: String) async throws {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        try await user.updatePassword(to: password)
     }
     
     func signOut() throws {
